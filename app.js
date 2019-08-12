fab5327e1fad9a411e8a26e05c62cc36ee28b7d0
sylbee <gustn5905@naver.com>
2019-08-13 01:21:09 +0900

    mongo db connected

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var mongoose = require('mongoose');
+
+mongoose.connect("mongodb+srv://test:testtest@cluster0-er6ge.mongodb.net/test?retryWrites=true&w=majority");
+var db = mongoose.connection;
+db.once("open",function() {
+  console.log("DB connected!");
+});
+db.on("error",function(){
+  console.log("DB ERROR :",err);
+});
 
 app.set("view engine", 'ejs');
-app.use(express.static(path.join(__dirname + '/public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 var data={count:0};
 app.get('/', function (req,res) {
@@ -14,11 +24,15 @@ app.get('/reset',function (req,res){
   data.count=0;
   res.render('my_first_ejs',data);
 });
-app.get('/set/count', function(req,res) {
+app.get('/set/count', function (req,res) {
   if(req.query.count) data.count=req.query.count;
   res.render('my_first_ejs',data);
 });
 app.get('/set/:num', function(req,res) {
   data.count=req.params.num;
   res.render('my_first_ejs',data);
+});
+
+app.listen(3000, function(){
+  console.log("server on!");
 });
