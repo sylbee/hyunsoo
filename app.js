c534d376b85e94fb7efa58d3ac0f3bf1e31b80a9
sylbee <gustn5905@naver.com>
2019-08-13 01:40:59 +0900

    db 아디/패숴드 재설정필요

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var path = require('path');
 var app = express();
 var mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://test:testtest@cluster0-er6ge.mongodb.net/test?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://id:password@cluster0-er6ge.mongodb.net/test?retryWrites=true&w=majority");
 var db = mongoose.connection;
 db.once("open",function() {
   console.log("DB connected!");
@@ -12,14 +12,34 @@ db.on("error",function(){
   console.log("DB ERROR :",err);
 });
 
+var dataSchema = mongoose.Schema({
+  name:String,
+  count:Number
+});
+var Data = mongoose.model('data',dataSchema);
+Data.findOne({name:"myData"},function(err,data){
+  if(err) return console.log("Data ERROR:",err);
+  if(!data){
+    Data.create({name:"myData",count:0},function (err,data) {
+      if(err) return console.log("Data ERROR",err);
+      console.log("Counter initialized: ",data);
+    });
+  };
+});
+
 app.set("view engine", 'ejs');
 app.use(express.static(path.join(__dirname, '/public')));
 
-var data={count:0};
 app.get('/', function (req,res) {
-  data.count++;
-  res.render('my_first_ejs',data);
+  Data.findOne({name:"myData"},function(err,data){
+    data.count++;
+    data.save(function (err) {
+      if(err) return console.log("Data ERROR",err);
+        res.render('my_first_ejs',data);
+      });
+    });
 });
+
 app.get('/reset',function (req,res){
   data.count=0;
   res.render('my_first_ejs',data);
